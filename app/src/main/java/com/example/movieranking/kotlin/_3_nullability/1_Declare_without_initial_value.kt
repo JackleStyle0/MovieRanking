ff92469d953cd42f3d1425a83f8ae7f5e4b737b3
Jckl3 <wacharat@odds.team>
2023-03-05 00:50:03 +0700

    Nullability

--- app/src/main/java/com/example/movieranking/kotlin/_3_nullability/1_Declare_without_initial_value.kt
+++ app/src/main/java/com/example/movieranking/kotlin/_3_nullability/1_Declare_without_initial_value.kt
@@ -2,8 +2,8 @@ package com.example.movieranking.kotlin._1_variable_and_types
 
 fun main() {
 
-    // Fails to compile
-//    var name: String = null
+    // Null can not be a value of a non-null type String
+    // var name: String = null
 
     var nameNullable: String? = null
 
