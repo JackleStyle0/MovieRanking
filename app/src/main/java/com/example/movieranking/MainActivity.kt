fa2a20a2156030a2a1e10cfb4e8d83228b350bac
Jckl3 <wacharat@odds.team>
2023-03-06 22:49:46 +0700

    Constraint example

--- app/src/main/java/com/example/movieranking/MainActivity.kt
+++ app/src/main/java/com/example/movieranking/MainActivity.kt
@@ -3,18 +3,17 @@ package com.example.movieranking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.movieranking.databinding.ActivityConstraintLayoutBinding
 import com.example.movieranking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+    private val binding: ActivityConstraintLayoutBinding by lazy {
+        ActivityConstraintLayoutBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
-        binding.textView.text = "Hello Android"
+        setContentView(R.layout.activity_constraint_layout_3)
     }
 }
